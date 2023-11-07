654e3baf79d4c2692b68e25fbc2444d390315654
Arthur Willian Assunção Francisco <arthur.francisco@pge.ro.gov.br>
2023-11-07 11:59:25 -0400

    .

--- script.js
+++ script.js
@@ -6,63 +6,92 @@
       alert("Camera API is not available in your browser");
       return;
     }
-    const video = document.querySelector('video');
-    const btnChangeCamera =document.querySelector('#btnChangeCamera')
-    
+  
+    // get page elements
+    const video = document.querySelector("#video");
+    const btnPlay = document.querySelector("#btnPlay");
+    const btnPause = document.querySelector("#btnPause");
+    const btnScreenshot = document.querySelector("#btnScreenshot");
+    const btnChangeCamera = document.querySelector("#btnChangeCamera");
+    const screenshotsContainer = document.querySelector("#screenshots");
+    const canvas = document.querySelector("#canvas");
+    const devicesSelect = document.querySelector("#devicesSelect");
+  
+    // video constraints
+    const constraints = {
+      video: {
+        width: {
+          min: 1280,
+          ideal: 1920,
+          max: 2560,
+        },
+        height: {
+          min: 720,
+          ideal: 1080,
+          max: 1440,
+        },
+      },
+    };
+  
+    // use front face camera
     let useFrontCamera = true;
-    
-
-    navigator.mediaDevices.getUserMedia({video:true})
-    .then(stream => {
-        video.srcObject = stream;
-        video.play();
-    })
-    .catch(error => {
-        console.log(error);
+  
+    // current video stream
+    let videoStream;
+  
+    // handle events
+    // play
+    btnPlay.addEventListener("click", function () {
+      video.play();
+      btnPlay.classList.add("is-hidden");
+      btnPause.classList.remove("is-hidden");
     });
-
-    document.querySelector('#tirarFoto').addEventListener('click', () => {
-        var canvas = document.querySelector('canvas');
-        canvas.height = video.videoHeight;
-        canvas.width = video.videoWidth;
-        var context = canvas.getContext('2d');
-        context.drawImage(video, 0, 0)
-        var link = document.createElement(nome);
-        link.download = 'fotp.png';
-        link.href = canvas.toDataURL();
-        link.textContent = 'Clique para baixar a imagem'
-        document.body.appendChild(link);
-        nome++;
+  
+    // pause
+    btnPause.addEventListener("click", function () {
+      video.pause();
+      btnPause.classList.add("is-hidden");
+      btnPlay.classList.remove("is-hidden");
     });
-
-
+  
+    // take screenshot
+    btnScreenshot.addEventListener("click", function () {
+      const img = document.createElement("img");
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext("2d").drawImage(video, 0, 0);
+      img.src = canvas.toDataURL("image/png");
+      screenshotsContainer.prepend(img);
+    });
+  
     // switch camera
     btnChangeCamera.addEventListener("click", function () {
-        useFrontCamera = !useFrontCamera;
-
-        initializeCamera();
+      useFrontCamera = !useFrontCamera;
+  
+      initializeCamera();
     });
+  
     // stop video stream
     function stopVideoStream() {
-        if (videoStream) {
-            videoStream.getTracks().forEach((track) => {
-            track.stop();
-            });
-        }
-        }
-
+      if (videoStream) {
+        videoStream.getTracks().forEach((track) => {
+          track.stop();
+        });
+      }
+    }
+  
     // initialize
     async function initializeCamera() {
-    stopVideoStream();
-    constraints.video.facingMode = useFrontCamera ? "user" : "environment";
-
-        try {
-            videoStream = await navigator.mediaDevices.getUserMedia(constraints);
-            video.srcObject = videoStream;
-        } catch (err) {
-            alert("Could not access the camera");
-        }
+      stopVideoStream();
+      constraints.video.facingMode = useFrontCamera ? "user" : "environment";
+  
+      try {
+        videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+        video.srcObject = videoStream;
+      } catch (err) {
+        alert("Could not access the camera");
+      }
     }
-
+  
     initializeCamera();
-})();
+  })();
