23d316ec019e5ad2d8062506ddb72a5007b1a31f
Arthur Willian Assunção Francisco <arthur.francisco@pge.ro.gov.br>
2023-11-07 11:44:28 -0400

    .

--- script.js
+++ script.js
@@ -1,59 +1,68 @@
-const video = document.querySelector('video');
-const btnChangeCamera =document.querySelector('#btnChangeCamera')
- 
-let useFrontCamera = true;
- 
+(function () {
+    if (
+      !"mediaDevices" in navigator ||
+      !"getUserMedia" in navigator.mediaDevices
+    ) {
+      alert("Camera API is not available in your browser");
+      return;
+    }
+    const video = document.querySelector('video');
+    const btnChangeCamera =document.querySelector('#btnChangeCamera')
+    
+    let useFrontCamera = true;
+    
 
-navigator.mediaDevices.getUserMedia({video:true})
-.then(stream => {
-    video.srcObject = stream;
-    video.play();
-})
-.catch(error => {
-    console.log(error);
-});
+    navigator.mediaDevices.getUserMedia({video:true})
+    .then(stream => {
+        video.srcObject = stream;
+        video.play();
+    })
+    .catch(error => {
+        console.log(error);
+    });
 
-document.querySelector('#tirarFoto').addEventListener('click', () => {
-    var canvas = document.querySelector('canvas');
-    canvas.height = video.videoHeight;
-    canvas.width = video.videoWidth;
-    var context = canvas.getContext('2d');
-    context.drawImage(video, 0, 0)
-    var link = document.createElement(nome);
-    link.download = 'fotp.png';
-    link.href = canvas.toDataURL();
-    link.textContent = 'Clique para baixar a imagem'
-    document.body.appendChild(link);
-    nome++;
-});
+    document.querySelector('#tirarFoto').addEventListener('click', () => {
+        var canvas = document.querySelector('canvas');
+        canvas.height = video.videoHeight;
+        canvas.width = video.videoWidth;
+        var context = canvas.getContext('2d');
+        context.drawImage(video, 0, 0)
+        var link = document.createElement(nome);
+        link.download = 'fotp.png';
+        link.href = canvas.toDataURL();
+        link.textContent = 'Clique para baixar a imagem'
+        document.body.appendChild(link);
+        nome++;
+    });
 
 
-// switch camera
-btnChangeCamera.addEventListener("click", function () {
-    useFrontCamera = !useFrontCamera;
+    // switch camera
+    btnChangeCamera.addEventListener("click", function () {
+        useFrontCamera = !useFrontCamera;
+
+        initializeCamera();
+    });
+    // stop video stream
+    function stopVideoStream() {
+        if (videoStream) {
+            videoStream.getTracks().forEach((track) => {
+            track.stop();
+            });
+        }
+        }
+
+    // initialize
+    async function initializeCamera() {
+    stopVideoStream();
+    constraints.video.facingMode = useFrontCamera ? "user" : "environment";
+
+        try {
+            videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+            video.srcObject = videoStream;
+        } catch (err) {
+            alert("Could not access the camera");
+        }
+    }
 
     initializeCamera();
-});
-// stop video stream
-function stopVideoStream() {
-    if (videoStream) {
-        videoStream.getTracks().forEach((track) => {
-        track.stop();
-        });
-    }
-    }
-
-// initialize
-async function initializeCamera() {
-stopVideoStream();
-constraints.video.facingMode = useFrontCamera ? "user" : "environment";
-
-    try {
-        videoStream = await navigator.mediaDevices.getUserMedia(constraints);
-        video.srcObject = videoStream;
-    } catch (err) {
-        alert("Could not access the camera");
-    }
-}
-
-initializeCamera();
+})();
