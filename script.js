2fcb5781f206a1345d8fca235cf1ef9573f54e59
Arthur Willian Assunção Francisco <arthur.francisco@pge.ro.gov.br>
2023-11-07 10:27:24 -0400

    .

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ document.querySelector('button').addEventListener('click', () => {
     var canvas = document.querySelector('canvas');
     canvas.height = video.videoHeight;
     canvas.width = video.videoWidth
+    var context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0)
 })
 
 async function getDevices() {
