c8d5161dd8964a3d8438998fd64828e0e9ce2119
Arthur Willian Assunção Francisco <arthur.francisco@pge.ro.gov.br>
2023-11-09 07:37:55 -0400

    .

--- localizaçao.js
+++ localizaçao.js
@@ -1,4 +1,4 @@
-const long = document.querySelector("#long")
+/*const long = document.querySelector("#long")
 const lati = document.querySelector("#lati")
 
 
@@ -29,13 +29,47 @@ function mostraLocalizaçao(pos){
     var map = new L.map('map', mapOption);
     
     //criando a camada
-    var layer = new L.TitleLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    var layer =  new TitleLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    
+    console.log(layer)
 
     map.addLayer(layer)
-}
+}*/
 
 
 function errorLocalizaçao() {
     console.log("Ero ao obter a localização")
 }
+
+let h2 = document.querySelector('h2');
+var map;
+
+function success(pos){
+    console.log(pos.coords.latitude, pos.coords.longitude);
+    h2.textContent = `Latitude:${pos.coords.latitude}, Longitude:${pos.coords.longitude}`;
+
+    if (map === undefined) {
+        map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 13);
+    } else {
+        map.remove();
+        map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 13);
+    }
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
+        .bindPopup('Eu estou aqui!')
+        .openPopup();
+}
+
+function error(err){
+    console.log(err);
+}
+
+var watchID = navigator.geolocation.watchPosition(success, error, {
+    enableHighAccuracy: true,
+    timeout: 5000
+});
 
