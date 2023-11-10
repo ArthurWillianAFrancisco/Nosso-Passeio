8e5c0b58150fd08f2afe092064d1ddc79cadd5ea
Arthur Willian Assunção Francisco <arthur.francisco@pge.ro.gov.br>
2023-11-10 13:18:47 -0400

    .

---
+++ api.js
@@ -0,0 +1,11 @@
+const ler_nome = document.getElementById("autor_nome")  
+const ler_cidade = document.getElementById("ler_cidade") 
+const ler_pais = document.getElementById("ler_pais")
+const ler_decricao = document.getElementById("ler_descricao") 
+const ler_nome_lugar = document.getElementById("ler_nome_lugar")  
+
+ler_nome.innerHTML = "Publicado por: "+" Artwill";
+ler_cidade.innerHTML = "Cidade: " + " Fuji em Shizuoka";
+ler_pais.innerHTML = "País: " + " Japão";
+ler_decricao.innerHTML = "Autamente recomendado, o acampamento tem um lago para pescar e uma otima vista do catão posta do japão o Monte Fuji (富士山). A Cidade de Fuji em Shizuoka, a apenas 30 quilômetros do Monte Fuji, é conhecida por seu esplêndido chá verde, super recomendo.";
+ler_nome_lugar.innerHTML = "Acampameto em frete ao monte Fuji";
