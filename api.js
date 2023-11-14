84ddf566c201af98b04dcb3f11c3a5d20e47c5de
Arthur Willian Assunção Francisco <arthur.francisco@pge.ro.gov.br>
2023-11-14 10:39:21 -0400

    index esta funcional

--- api.js
+++ api.js
@@ -1,7 +1,10 @@
+//let text = '{"employees":[' + '{"nome":" Artwil","nome_lugar":"Acampameto em frete ao Monte Fuji", "local_pais":" Japão", "cidade":" Fuji em Shizuoka", "descricao":"Autamente recomendado, o acampamento tem um lago para pescar e uma otima vista do catão posta do japão o Monte Fuji (富士山). A Cidade de Fuji em Shizuoka, a apenas 30 quilômetros do Monte Fuji, é conhecida por seu esplêndido chá verde, super recomendo.", "p_comentarista": "Alextacha", "comentario": "Que lindo vamos lá qualquer dia!", "lati": "-8.7579878", "long": "-63.8995221"}]}'
 
-let text = '{"employees":[' + '{"nome":" Artwil","nome_lugar":"Acampameto em frete ao Monte Fuji", "local_pais":" Japão", "cidade":" Fuji em Shizuoka", "descricao":"Autamente recomendado, o acampamento tem um lago para pescar e uma otima vista do catão posta do japão o Monte Fuji (富士山). A Cidade de Fuji em Shizuoka, a apenas 30 quilômetros do Monte Fuji, é conhecida por seu esplêndido chá verde, super recomendo.", "p_comentarista": "Alextacha", "comentario": "Que lindo vamos lá qualquer dia!", "lati": "-8.7579878", "long": "-63.8995221"}]}';
+const fs = require("fs")
 
-const nome_Auto = JSON.parse(text);
+const dadosAtuais = JSON.parse(fs.readFileSync("passeio.json"))
+
+const nome_Auto = JSON.parse(dadosAtuais);
 document.getElementById("autor_nome").innerHTML = "Publicado por: "+
 nome_Auto.employees[0].nome;
 
@@ -25,7 +28,6 @@ const comentarista = JSON.parse(text);
 document.getElementById("comentarista").innerHTML = 
 comentarista.employees[0].p_comentarista  + " comentou: ";
 
-
 const comentario = JSON.parse(text);
 document.getElementById("comentario").innerHTML = 
 comentario.employees[0].comentario;
@@ -37,3 +39,5 @@ latitude.employees[0].lati;
 const longitude = JSON.parse(text);
 document.getElementById("long").innerHTML = 
 longitude.employees[0].long;
+
+
--- ler_passeio.js
+++ ler_passeio.js
@@ -3,11 +3,13 @@ const nome_lugar = document.getElementById("nome_lugar").innerHTML
 const pais = document.getElementById("pais").innerHTML
 const cidade = document.getElementById("cidade").innerHTML
 const descricao = document.getElementById("decricao").innerHTML
+const lati = document.getElementById("lati").innerHTML
+const long = document.getElementById("long").innerHTML
 
 const fs = require("fs")
 
 const dadosAtuais = JSON.parse(fs.readFileSync("passeio.json"))
-const novoPasseio = {nome: nome_auto, nome_lugar: nome_lugar, cidade: cidade, pais: pais, descricao: descricao}
+const novoPasseio = {nome: nome_auto, nome_lugar: nome_lugar, cidade: cidade, pais: pais, descricao: descricao, latitude: lati, logitude: long}
 
 
 fs.writeFileSync("passeio.json", JSON.stringify([...dadosAtuais, novoPasseio]))
